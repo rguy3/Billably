625f67213007b6725912e0c4f1c391b44c5b1f00
Blu <Blu@admins-macbook-pro-3.local>
2017-03-14 22:35:09 -0400

    Initial Commit

---
+++ Billably/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Billably
+//
+//  Created by Blu on 3/14/17.
+//  Copyright © 2017 Blu. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Billably/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Billably
+//
+//  Created by Blu on 3/14/17.
+//  Copyright © 2017 Blu. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
